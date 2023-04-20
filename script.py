955193e47fc4b608102d973e9f94d831ea6d3a6f
dulap16 <tudorbrk8@yahoo.com>
2023-04-21 01:09:00 +0300

    add files

---
+++ script.py
@@ -0,0 +1,120 @@
+import serial.tools.list_ports
+from pydub import AudioSegment
+from pydub.playback import play
+import pygame
+from threading import Thread
+import ffmpeg
+import time
+
+ports = serial.tools.list_ports.comports()
+
+serialInst = serial.Serial()
+
+pygame.init()
+pygame.mixer.init()
+
+
+kick = pygame.mixer.Sound(r'C:\\Users\tudor\OneDrive\Documents\Arduino\python_scripts\Beat-Maker-3000\sounds\Pierre_Kick.wav')
+clap = pygame.mixer.Sound(r'C:\\Users\tudor\OneDrive\Documents\Arduino\python_scripts\Beat-Maker-3000\sounds\Basic_Clap.wav')
+hihat = pygame.mixer.Sound(r'C:\\Users\tudor\OneDrive\Documents\Arduino\python_scripts\Beat-Maker-3000\sounds\Pierre_Hat.wav')
+snare = pygame.mixer.Sound(r'C:\\Users\tudor\OneDrive\Documents\Arduino\python_scripts\Beat-Maker-3000\sounds\Pierre_Snare.wav')
+openhat = pygame.mixer.Sound(r'C:\\Users\tudor\OneDrive\Documents\Arduino\python_scripts\Beat-Maker-3000\sounds\Open_Hat.wav')
+
+numberOfSounds = 5
+List = [kick, hihat, clap, snare, openhat]
+names = ["KICK", "HIHAT", "CLAP", "SNARE", "OPENHAT"]
+
+
+soundMatrix = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+               [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+               [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+               [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+               [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+               [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+               [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+               [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], 
+               [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+
+soundsPlayed = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+def music():
+    while True:
+        for beat in range(1, 17):
+
+            for sound in range(0, numberOfSounds):
+                if(soundMatrix[sound][beat]):
+                    soundsPlayed[sound] = 1
+            
+            for i in range(0, numberOfSounds):
+                if soundsPlayed[i] == 1:
+                    List[i].play()
+                soundsPlayed[i] = 0
+            time.sleep(0.2)
+
+def equalizeLines(word):
+    l = len(word)
+
+    return lengthiestOutOfSounds() - l
+
+def lengthiestOutOfSounds():
+    maxi = 0
+    for i in range(0, numberOfSounds):
+        maxi = max(maxi, len(names[i]))
+    
+    return maxi
+
+
+def readSerial():
+
+    serialInst.baudrate = 9600
+    serialInst.port = "COM3"
+    serialInst.open()
+    
+    while True:
+        if serialInst.in_waiting:
+            packet = serialInst.readline()
+            line = (packet.decode('utf')).rstrip('\n')
+
+            if line[0] >= '0' and line[0] <= '9':
+                first, second = line.split(' ')
+                row = int(first)
+                column = int(second)
+
+                if soundMatrix[row][column] == True:
+                    soundMatrix[row][column] = False
+                else: 
+                    soundMatrix[row][column] = True
+            else:
+                tokens = line.split(' ')
+
+                ok = False
+                for i in range(0, numberOfSounds):
+                    if tokens[0] == names[i]:
+                        ok = True
+                        i = numberOfSounds
+
+                if ok == True:
+                    print(tokens[0], end = " ", flush = True)
+
+                    spaces = equalizeLines(tokens[0])
+                    print(" " * spaces, end = "", flush = True)
+
+                    for i in range(1, 17):
+                        if tokens[i] == '1': 
+                            print("▮", end = "", flush = True)
+                        else: print("▯", end = "", flush = True)
+                    print()
+                
+                if tokens[0] == names[numberOfSounds - 1]:
+                    print()
+                    print()
+
+
+if __name__ == "__main__":
+    read = Thread(target = readSerial)
+    makeMusic = Thread(target = music)
+
+    read.start()
+    makeMusic.start()
+
+    serialInst.close()
---
+++ soundpadTEST.ino
@@ -0,0 +1,138 @@
+#include <Wire.h>
+#include <Keypad.h>
+#include <LiquidCrystal_I2C.h>
+
+#define changeSoundInput A0
+#define printBeatInput A1
+#define changeSoundOutput 13
+#define printBeatOutput 12
+
+const int nrOfSounds = 5; // CHANGE IF MORE SOUNDS ADDED
+char sounds[20][20] = {"KICK ", "HIHAT ", "CLAP ", "SNARE ", "OPENHAT "}; // ADD SOUNDS HERE
+char printedSound[20];
+
+// 4X4 KEYPAD INITIALISATION - chech 
+const byte ROWS = 4;
+const byte COLS = 4;
+
+byte cols[COLS] = {5, 4, 3, 2}; // COLUMN PINS
+byte rows[ROWS] = {6, 7, 8, 9};
+char keys[ROWS][COLS] = {
+  {'1', '2', '3', '4'}, 
+  {'5', '6', '7', '8'}, 
+  {'9', 'A', 'B', 'C'}, 
+  {'D', 'E', 'F', 'G'}, 
+};
+
+Keypad keypad = Keypad(makeKeymap(keys), rows, cols, ROWS, COLS);
+
+// I2C INITIALISATION
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+// 0 - kick
+// 1 - hihat
+// 2 - clap
+// 3 - snare
+// 4 - openhats
+// add more if you'd like
+
+int currentSound = 0;
+bool soundMatrix[nrOfSounds + 2][17];
+
+bool changeOff = true;
+bool showGridOff = true;
+int timer = 0;
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+
+  pinMode(changeSoundOutput, OUTPUT);
+  pinMode(printBeatOutput, OUTPUT);
+
+  digitalWrite(changeSoundOutput, HIGH);
+  digitalWrite(printBeatOutput, HIGH);
+
+
+  lcd.init();
+  lcd.backlight();
+  lcd.setCursor(0, 0);
+  lcd.print(sounds[0]);
+}
+
+
+void loop() {
+  char pressed = keypad.getKey();
+
+  if(pressed) {
+    int i = hexaToInt(pressed);
+    Serial.print(currentSound);
+    Serial.print(" ");
+    Serial.println(hexaToInt(pressed));
+    soundMatrix[currentSound][i] = !(soundMatrix[currentSound][i]);
+
+    lcd.setCursor(i - 1, 1);
+    if(soundMatrix[currentSound][i])
+      lcd.write(255);
+    else lcd.print(" ");
+  }
+
+  if(analogRead(changeSoundInput))
+  {
+    if(changeOff) {
+      changeOff = false;
+      currentSound = (currentSound + 1) % nrOfSounds;
+
+      // CHANGE DISPLAY
+
+      lcd.setCursor(0, 0);
+      strcpy(printedSound, sounds[currentSound]);
+      strcat(printedSound, "   ");
+      lcd.print(printedSound);
+
+      
+      for(int i = 1; i <= 16; i++)
+      {
+        lcd.setCursor(i - 1, 1);
+        if(soundMatrix[currentSound][i])
+          lcd.write(255);
+        else lcd.print(" ");
+      }
+
+      Serial.println();
+    }
+  } else changeOff = true;
+
+  if(analogRead(printBeatInput))
+  {
+    if(showGridOff) {
+      showGridOff = false;
+      printSounds();
+    }
+  } else showGridOff = true;
+
+  delay(10);
+}
+
+int hexaToInt(char c)
+{
+  if(c >= 'A')
+    return c - 'A' + 10;
+  return c - '0';
+}
+
+void printSounds()
+{
+  for(int i = 0; i < nrOfSounds; i++)
+  {
+    Serial.print(sounds[i]);
+    for(int j = 1; j <= 16; j++) {
+      Serial.print(soundMatrix[i][j]);
+      Serial.print(" ");
+    }
+    Serial.println();
+  }
+  Serial.println();
+}
